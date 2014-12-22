b3f45693e1c732d8ffb23e3ae6fd9cfaba9ab906
Tim Creech <tcreech@umd.edu>
2014-12-21 20:53:55 -0500

    add options for finding osxfuse, and a warning if neither is specified

--- mfs.rb
+++ mfs.rb
@@ -5,9 +5,14 @@ class Mfs < Formula
   url "http://ppa.moosefs.com/src/moosefs-ce-2.0.43-1.tar.gz"
   sha1 "eb82cec7fbc329c43309342a9738be64d90d6ec0"
 
+  option "with-osxfuse", "Use Homebrew's osxfuse for mfsmount"
   depends_on "osxfuse" => :optional
 
+  option "with-pkg-config", "Use pkg-config to find an external osxfuse for mfsmount"
+  depends_on "pkg-config" => :optional
+
   def install
+    opoo "WARNING: Not likely to build mfsmount without osxfuse or pkg-config" unless ( build.with? "osxfuse" or build.with? "pkg-config" )
 
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
